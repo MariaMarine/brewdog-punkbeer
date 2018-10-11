5b6967d52a5a7b0f326b51966629df93626b6253
Maria Marinova <maria.marinova@gmail.com>
2018-10-11 20:21:28 +0300

    Minor styling on beer catalogue

--- src/javaScript/templating.js
+++ src/javaScript/templating.js
@@ -2,9 +2,9 @@ const createBeerTemplate = (data) => {
   return `
         <div id=${data.id} class="col-sm-4 beerThumbnail">
         <div class="panel panel-success">
-          <div class="panel-heading">${data.id}</div>
+          <div class="panel-heading"><strong>${data.name}</strong></div>
           <div class="panel-body"><img src="${data.image_url}" class="img-responsive"  alt="${data.name}"></div>
-          <div class="panel-footer"><strong>${data.name}</strong></div>
+          <div class="panel-footer">${data.tagline} <span id="right-icon" class="glyphicon glyphicon-chevron-right"></span></div>
         </div>
         </div>
         `;
@@ -36,7 +36,7 @@ const createFavouritesTemplate = (data) => {
   return `
       <div id=${data.id} class="col-sm-4 favBeerThumbnail">
       <div class="panel panel-success">
-        <div id="remove-beer" class="panel-footer"> Remove <span id="remove-icon" class="glyphicon glyphicon-remove"></span></div>
+        <div id="remove-beer" class="panel-footer"> Remove <span id="right-icon" class="glyphicon glyphicon-remove"></span></div>
         <div class="panel-heading"><strong>${data.name}</strong></div>
         <div class="panel-body"><img src="${data.image_url}" class="img-responsive"  alt="${data.name}"></div>
         </div>

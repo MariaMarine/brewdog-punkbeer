82f6921e44e4e3ebaabe76ce761ff68bf203f902
Lubomir Oresharov <l.oresharov@gmail.com>
2018-10-12 14:11:44 +0300

    added filters

---
+++ src/javaScript/searchAndFilter.js
@@ -0,0 +1,23 @@
+import { state } from './state.js';
+import * as $ from 'jquery';
+const createRequestUrl = (state) => {
+    let requestUrl = `https://api.punkapi.com/v2/beers`;
+
+    
+
+    // create the request
+return requestUrl;
+};
+
+
+// const alcoholSearch = function(){
+//     $('#random').click(function () {
+//         $.get(createRequestUrl(), function (data, status) {
+//               console.log(data)
+//     });
+//   })
+// ;};
+
+//export { alcoholSearch };
+
+

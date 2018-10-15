36ca01e95b3b571a1ad58476d66339b2247a92a3
Nikola Zlatinov <nizlatinov@gmail.com>
2018-10-15 22:21:05 +0300

    added babel

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,13 @@ module.exports = {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist')
   },
+  module: {
+    rules: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      loader: "babel-loader"
+    }]
+  },
 
   devtool: 'source-map'
 };
